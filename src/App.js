acb9eb94cc796e47f783b0df49f5c41f0e764ba1
Keith Charlton <charltonkeith8@gmail.com>
2025-01-28 19:43:59 -0500

    added icon & debugging

--- src/App.js
+++ src/App.js
@@ -306,9 +306,9 @@ function Movielist({ movieSelected, setMovieSelected, children }) {
 }
 
 function Watchedlist({ watched, setWatched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating)) || "";
+  const avgUserRating = average(watched.map((movie) => movie.userRating)) || "";
+  const avgRuntime = average(watched.map((movie) => movie.runtime)) || "";
 
   function handleRemoveMovie(id) {
     setWatched(watched.filter((movie) => movie.imdbID !== id));
